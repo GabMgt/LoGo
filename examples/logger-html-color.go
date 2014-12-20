8b9d947d8a8a57023fdddbd92ec309a042d66065
Nurza <gabriel.mgt0+git@gmail.com>
2014-12-20 14:06:10 +0100

    Add HTMLColor

---
+++ examples/logger-html-color.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+	Example of a Logger with console colors
+*/
+
+import (
+	".."
+	// logo "github.com/nurza/logo"
+)
+
+func main() {
+	var l logo.Logger                          // Create a simple Logger
+	t := l.AddTransport(logo.File, "test.log") // Add a transport: Console
+	t.AddColor(logo.HTMLColor)                 // Add a color: Console color
+	l.EnableAllLevels()                        // Enable all logging levels
+
+	l.Silly("Silly")
+	l.Debug("Debug")
+	l.Verbose("Verbose")
+	l.Info("Info")
+	l.Warn("Warn")
+	l.Error("Error")
+	l.Critical("Critical")
+}
