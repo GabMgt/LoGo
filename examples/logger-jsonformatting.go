18157d1ef1e3173cdc0f2cff87322a1c56f88afa
Nurza <gabriel.mgt0@gmail.com>
2017-04-06 10:56:24 +0200

    Add example for JSON formatting to transport

---
+++ examples/logger-jsonformatting.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+	Example of the feature: enable json formatting
+*/
+
+import (
+	logo "github.com/nurza/logo"
+)
+
+func main() {
+	var l logo.Logger                                // Create a simple Logger
+	t1 := l.AddTransport(logo.Console)               // Add a transport: Console
+	t1.AddColor(logo.ConsoleColor)                   // Add a color: Console color
+	t2 := l.AddTransport(logo.File, "test.log.json") // Add a transport: File
+	t2.JSONFormatted = true                          // Enable JSON formatting
+	l.EnableAllLevels()                              // Enable all logging levels
+	l.SetPrefix("{ ")                                // Set Prefix
+	l.SetSuffix(" }")                                // Set Suffix
+	l.AddTime("2006-01-02 15:04:05")                 // Add time template
+	l.GoRoutineId = true                             // Add Go routine id
+
+	l.Silly("Silly")
+	l.Debug("Debug")
+	l.Verbose("Verbose")
+	l.Info("Info")
+	l.Warn("Warn")
+	l.Error("Error")
+	l.Critical("Critical")
+}
